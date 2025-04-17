bedfa94bdd79f54492c4bee11a0811c6c447450d
Evandro Lugli <78626020+evandrolugli@users.noreply.github.com>
2025-04-17 20:04:31 +1000

    add auth

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+
+- Next.js
+- Auth.js
+    - npx auth secret
+- Auth Google
+
 
 ## Getting Started
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4x1 font-bold">Hi There</h1>
+      <h1 className="text-4x1 font-bold">Landing Page</h1>
+      <Link href="/login">
+        <button>Login</button>
+      </Link>
     </div>
   );
 }
